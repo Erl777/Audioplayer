5e14174be8c5d99faf231aaa1478bf7e0aa6f3fe
Erl777 <pokemon.322.go@gmail.com>
2020-01-30 14:45:58 +0300

    время проигрывания трека, название трека

--- Audioplayer.js
+++ Audioplayer.js
@@ -15,7 +15,10 @@ class Audioplayer {
         this.id = 0;
         this.playlist = null;
         this.once = false;
-
+        this.timeElem = document.getElementById('timeleft');
+        this.minutes = 0;
+        this.seconds = 0;
+        this.timer = null;
         this.initialization();
     }
 
@@ -34,6 +37,7 @@ class Audioplayer {
         this.song.setAttribute('data-id', this.id);
         this.addPlayList();
         this.addTimeUpdateEvent();
+        this.getSongName();
     }
 
     addPlayList(){
@@ -56,12 +60,20 @@ class Audioplayer {
         if(this.once === false){
             this.input.max = document.getElementById('track').duration;
             this.once = true;
+            this.countTime();
+            this.timeReduction();
         }
+        if(this.timer === false){
+            this.timeReduction();
+        }
+
         this.song.play();
     };
 
     stopPlay = () =>{
         this.song.pause();
+        clearInterval(this.timer);
+        this.timer = false;
     };
 
     setTime = () =>{
@@ -69,14 +81,56 @@ class Audioplayer {
         if(this.song.ended){
             this.nextSong();
         }
+        if(this.song.ended && this.id === this.playlist.length) clearInterval(this.timer);
     };
+
+    timeReduction(){
+        let fullTime = parseInt(this.song.duration, 10);
+        let newTime = fullTime - this.input.value;
+        this.minutes = parseInt((newTime / 60), 10);
+        this.seconds = newTime - (this.minutes * 60);
+        let localMinutes;
+        this.timer = setInterval(() =>{
+            if(this.seconds < 1){
+                this.minutes--;
+                this.seconds = 60;
+            }
+            this.seconds--;
+
+            if(this.minutes < 10){
+                localMinutes = '0' + this.minutes;
+            }
+            if(this.seconds < 10){
+                this.seconds = '0' + this.seconds;
+            }
+            this.timeElem.textContent = (`${localMinutes}:${this.seconds}`);
+        },1000);
+
+    }
+
+    countTime(){
+        let fullTime = parseInt(this.song.duration, 10);
+        this.minutes = parseInt((fullTime / 60), 10);
+        this.seconds = fullTime - (this.minutes * 60);
+        let localMinutes;
+        if(this.minutes < 10){
+            localMinutes = '0' + this.minutes;
+        }
+        if(this.seconds < 10){
+            this.seconds = '0' + this.seconds;
+        }
+        let time = document.getElementById('timeleft');
+        time.textContent = (`${localMinutes}:${this.seconds}`);
+    }
 
     chooseValue = () => {
       this.song.removeEventListener('timeupdate', this.setTime);
+      clearInterval(this.timer);
     };
 
     changeValue = () =>{
         this.song.currentTime = this.input.value;
+        this.timeReduction();
         this.song.addEventListener('timeupdate', this.setTime);
     };
 
@@ -89,7 +143,12 @@ class Audioplayer {
             this.song = this.playlist[this.id];
             this.song.currentTime = 0;
             this.input.max = this.song.duration;
+            this.countTime();
+            this.getSongName();
             this.song.play();
+        }
+        if(this.timer === false){
+            this.timeReduction();
         }
 
     };
@@ -103,15 +162,28 @@ class Audioplayer {
             this.song = this.playlist[this.id];
             this.song.currentTime = 0;
             this.input.max = this.song.duration;
+            this.countTime();
+            this.getSongName();
             this.song.play();
         }
 
+        if(this.song.ended && this.id === (this.playlist.length - 1)){
+            clearInterval(this.timer);
+            this.timer = false;
+        }
+
+    };
+
+    getSongName(){
+        let elem = document.getElementById('trackName');
+        let b = this.song.src.substr(this.song.src.lastIndexOf('/')+1);
+        elem.textContent = b.split('.')[0];
     }
 
 }
 
 new Audioplayer({
-    playlist: ['music/TutTutChild_HotPursuit.mp3',
-        'music/ac-dc-i-love-rock-and-roll(mp3-top.info).mp3',
-        'music/tones-and-i-dance-monkey(mp3-top.info).mp3']
+    playlist: ['music/halogen-u-got-that.mp3','music/TutTutChild_HotPursuit.mp3',
+        'music/ac-dc-i-love-rock-and-roll.mp3',
+        'music/tones-and-i-dance-monkey.mp3']
 });
